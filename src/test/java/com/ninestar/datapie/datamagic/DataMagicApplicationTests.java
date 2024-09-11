660e24ad1361ca93545e8f85445ef944ab6acd5a
Gavin Zhao <jichun.zhao@outlook.com>
2024-09-11 11:29:36 -0400

    remove: some test cases

--- src/test/java/com/ninestar/datapie/datamagic/DataMagicApplicationTests.java
+++ src/test/java/com/ninestar/datapie/datamagic/DataMagicApplicationTests.java
@@ -23,42 +23,8 @@ public class DataMagicApplicationTests {
 	private MockMvc mockMvc;
 
 	@Test
-	public void loginWithValidUserThenAuthenticated() throws Exception {
-		FormLoginRequestBuilder login = formLogin()
-			.user("user")
-			.password("password");
-
-		mockMvc.perform(login)
-			.andExpect(authenticated().withUsername("user"));
-	}
-
-	@Test
-	public void loginWithInvalidUserThenUnauthenticated() throws Exception {
-		FormLoginRequestBuilder login = formLogin()
-			.user("invalid")
-			.password("invalidpassword");
-
-		mockMvc.perform(login)
-			.andExpect(unauthenticated());
-	}
-
-	@Test
-	public void accessUnsecuredResourceThenOk() throws Exception {
-		mockMvc.perform(get("/"))
-			.andExpect(status().isOk());
-	}
-
-	@Test
-	public void accessSecuredResourceUnauthenticatedThenRedirectsToLogin() throws Exception {
-		mockMvc.perform(get("/hello"))
-			.andExpect(status().is3xxRedirection())
-			.andExpect(redirectedUrlPattern("**/login"));
-	}
-
-	@Test
 	@WithMockUser
 	public void accessSecuredResourceAuthenticatedThenOk() throws Exception {
-		mockMvc.perform(get("/hello"))
-				.andExpect(status().isOk());
+		mockMvc.perform(get("/main")).andExpect(status().isOk());
 	}
 }
