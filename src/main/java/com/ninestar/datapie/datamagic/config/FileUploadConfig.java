472a5dc1f6edb71ef98cbfeff57086bb97f00c50
Gavin Zhao <jichun.zhao@outlook.com>
2025-01-14 21:31:50 -0500

    update: upload folder

--- src/main/java/com/ninestar/datapie/datamagic/config/FileUploadConfig.java
+++ src/main/java/com/ninestar/datapie/datamagic/config/FileUploadConfig.java
@@ -4,11 +4,15 @@ import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.unit.DataSize;
-
+import org.springframework.beans.factory.annotation.Value;
 import javax.servlet.MultipartConfigElement;
+import java.io.File;
 
 @Configuration
 public class FileUploadConfig {
+    @Value("${spring.servlet.multipart.location}")
+    private String location;
+
     @Bean
     public MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
@@ -17,6 +21,13 @@ public class FileUploadConfig {
         // total size of uploading (G)
         factory.setMaxRequestSize(DataSize.ofGigabytes(10L));
 
+        // set the path of tmp
+        String uploadDir = System.getProperty("user.dir") + location;
+        File file = new File(uploadDir);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+        factory.setLocation(uploadDir);
         return factory.createMultipartConfig();
     }
 }
